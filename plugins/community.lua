3126f1400e719766e92a26156d4db5994e2680eb
saiwingy AIP <saiwingy@zillowgroup.com>
2023-08-11 14:14:14 -0700

    Add Navbuddy and Telescope grep with args

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,6 +6,7 @@ return {
 
   { import = "astrocommunity.colorscheme.catppuccin", lazy = false },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
 
   -- Can't get this to work - got b unch of errors
   -- { import = "astrocommunity.completion.copilot-lua" },
