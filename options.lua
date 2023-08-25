5c2f57cdad596ee3b709cd3dcfa8442a5d5e03c3
saiwingy AIP <saiwingy@zillowgroup.com>
2023-08-25 11:52:49 -0700

    Don't want swapfiles

--- options.lua
+++ options.lua
@@ -7,6 +7,9 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+
+    -- SWY
+    swapfile = false,
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
