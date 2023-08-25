adf6afeffe69c7076051c31eb2f5086dba693d78
Saiwing Yeung <saiwingy@zillowgroup.com>
2023-08-25 13:17:01 -0700

    Add todo-comments.nvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,18 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+
+  -- This actually works
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup {}
+    end,
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" }
+    }
+  },
 }
